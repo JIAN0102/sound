1b03b83795c05adcee6aa68b3857b15a5085647b
JL Chen <azx011014@gmail.com>
2022-11-03 14:29:55 +0800

    feat: setting rules and locale

--- src/components/AuthModal.vue
+++ src/components/AuthModal.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, reactive } from 'vue';
+import { ref } from 'vue';
 import { useModalStore } from '@/stores/modal';
 import { storeToRefs } from 'pinia';
 
@@ -8,17 +8,20 @@ const { isOpen } = storeToRefs(modalStore);
 const { toggleAuthModal } = modalStore;
 
 const type = ref('login');
-const schema = reactive({
-  email: 'required|email',
-  password: 'required|min:6|max:32',
-});
+
+function login(values) {
+  console.log(values);
+}
+
+function register(values) {
+  console.log(values);
+}
 </script>
 
 <template>
   <div
-    class="fixed inset:0 z:9999 overflow-x:hidden overflow-y:auto bg:repeating-linear-gradient(40deg,#19191e,#19191e|3px,white/.1|3px,white/.1|8px) bd:blur(5) opacity:0 invisible ~opacity|.3s,visibility|.3s {opacity:1;visible}.is-open"
+    class="fixed inset:0 z:9999 overflow-x:hidden overflow-y:auto pointer-events:none bg:repeating-linear-gradient(40deg,#19191e,#19191e|3px,white/.1|3px,white/.1|8px) bd:blur(5) opacity:0 invisible ~opacity|.3s,visibility|.3s {pointer-events:auto;opacity:1;visible}.is-open"
     :class="{ 'is-open': isOpen }"
-    @click.prevent.self="toggleAuthModal"
   >
     <div
       class="flex ai:center max-w:640 min-h:calc(100%-80px) px:20 m:40|auto pointer-events:none"
@@ -59,30 +62,38 @@ const schema = reactive({
           </div>
         </div>
         <div class="p:40 bg:#333 b:10|solid|black r:60">
-          <VeeForm v-show="type === 'login'" :validation-schema="schema">
+          <VForm v-show="type === 'login'" @submit="login">
             <div>
-              <label class="f:bold f:20 fg:white" for="loginEmail"
+              <label class="f:bold f:18 fg:white" for="loginEmail"
                 >電子郵件</label
               >
-              <VeeField
+              <VField
                 id="loginEmail"
                 class="block w:full h:60 px:26 mt:10 fg:white bg:black b:3|solid|transparent rounded outline:0 b:#f4db0d:focus"
                 type="email"
-                name="email"
+                name="電子郵件"
+                rules="required|email"
               />
-              <ErrorMessage class="fg:red" name="email" />
+              <ErrorMessage
+                class="inline-block p:6|12 mt:10 f:12 fg:white bg:#ff3333 rounded"
+                name="電子郵件"
+              />
             </div>
             <div class="mt:20">
-              <label class="f:bold f:20 fg:white" for="loginPassword"
+              <label class="f:bold f:18 fg:white" for="loginPassword"
                 >密碼</label
               >
-              <VeeField
+              <VField
                 id="loginPassword"
                 class="block w:full h:60 px:26 mt:10 fg:white bg:black b:3|solid|transparent rounded outline:0 b:#f4db0d:focus"
                 type="password"
-                name="password"
+                name="密碼"
+                rules="required|min:6|max:100"
               />
-              <ErrorMessage class="fg:red" name="password" />
+              <ErrorMessage
+                class="inline-block p:6|12 mt:10 f:12 fg:white bg:#ff3333 rounded"
+                name="密碼"
+              />
             </div>
             <button
               class="rel w:full h:60 mt:40 f:bold f:20 bg:linear-gradient(to|right,#fd9d02,#f4db0d) rounded"
@@ -93,8 +104,82 @@ const schema = reactive({
               ></span>
               登入
             </button>
-          </VeeForm>
-          <VeeForm v-show="type === 'register'"></VeeForm>
+          </VForm>
+          <VForm v-show="type === 'register'" @submit="register">
+            <div>
+              <label class="f:bold f:18 fg:white" for="registerName"
+                >我們該如何稱呼你？</label
+              >
+              <VField
+                id="registerName"
+                class="block w:full h:60 px:26 mt:10 fg:white bg:black b:3|solid|transparent rounded outline:0 b:#f4db0d:focus"
+                type="text"
+                name="姓名"
+                rules="required"
+              />
+              <ErrorMessage
+                class="inline-block p:6|12 mt:10 f:12 fg:white bg:#ff3333 rounded"
+                name="姓名"
+              />
+            </div>
+            <div class="mt:20">
+              <label class="f:bold f:18 fg:white" for="registerEmail"
+                >你的電子郵件是什麼？</label
+              >
+              <VField
+                id="registerEmail"
+                class="block w:full h:60 px:26 mt:10 fg:white bg:black b:3|solid|transparent rounded outline:0 b:#f4db0d:focus"
+                type="email"
+                name="電子郵件"
+                rules="required|email"
+              />
+              <ErrorMessage
+                class="inline-block p:6|12 mt:10 f:12 fg:white bg:#ff3333 rounded"
+                name="電子郵件"
+              />
+            </div>
+            <div class="mt:20">
+              <label class="f:bold f:18 fg:white" for="registerPassword"
+                >密碼</label
+              >
+              <VField
+                id="registerPassword"
+                class="block w:full h:60 px:26 mt:10 fg:white bg:black b:3|solid|transparent rounded outline:0 b:#f4db0d:focus"
+                type="password"
+                name="密碼"
+                rules="required|min:6|max:100"
+              />
+              <ErrorMessage
+                class="inline-block p:6|12 mt:10 f:12 fg:white bg:#ff3333 rounded"
+                name="密碼"
+              />
+            </div>
+            <div class="mt:20">
+              <label class="f:bold f:18 fg:white" for="registerConfirmPassword"
+                >確認密碼</label
+              >
+              <VField
+                id="registerConfirmPassword"
+                class="block w:full h:60 px:26 mt:10 fg:white bg:black b:3|solid|transparent rounded outline:0 b:#f4db0d:focus"
+                type="password"
+                name="確認密碼"
+                rules="confirmed:@密碼"
+              />
+              <ErrorMessage
+                class="inline-block p:6|12 mt:10 f:12 fg:white bg:#ff3333 rounded"
+                name="確認密碼"
+              />
+            </div>
+            <button
+              class="rel w:full h:60 mt:40 f:bold f:20 bg:linear-gradient(to|right,#fd9d02,#f4db0d) rounded"
+              type="submit"
+            >
+              <span
+                class="abs top:1/2 left:10 w:40 h:40 b:4|solid|black rounded translateY(-50%)"
+              ></span>
+              註冊
+            </button>
+          </VForm>
         </div>
       </div>
     </div>
--- src/includes/validation.js
+++ src/includes/validation.js
@@ -1,29 +1,30 @@
 import {
-  Form as VeeForm,
-  Field as VeeField,
+  Form as VForm,
+  Field as VField,
   ErrorMessage,
   defineRule,
+  configure,
 } from 'vee-validate';
-import {
-  required,
-  min,
-  max,
-  alpha_spaces as alphaSpaces,
-  email,
-  confirmed,
-} from '@vee-validate/rules';
+import { required, min, max, email, confirmed } from '@vee-validate/rules';
+import { localize, setLocale } from '@vee-validate/i18n';
+import zhTW from '@vee-validate/i18n/dist/locale/zh_TW.json';
 
 export default {
   install(app) {
-    app.component('VeeForm', VeeForm);
-    app.component('VeeField', VeeField);
+    app.component('VForm', VForm);
+    app.component('VField', VField);
     app.component('ErrorMessage', ErrorMessage);
 
     defineRule('required', required);
     defineRule('min', min);
     defineRule('max', max);
-    defineRule('alpha_spaces', alphaSpaces);
     defineRule('email', email);
-    defineRule('password_mismatch', confirmed);
+    defineRule('confirmed', confirmed);
+
+    configure({
+      generateMessage: localize({ zh_TW: zhTW }),
+    });
+
+    setLocale('zh_TW');
   },
 };
