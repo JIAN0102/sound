385bb2c0027d07419e4df31f58b590c98edd2c67
JL Chen <azx011014@gmail.com>
2022-11-18 01:02:10 +0800

    feat: adjustment loading

--- src/components/TheLoading.vue
+++ src/components/TheLoading.vue
@@ -18,20 +18,18 @@ watch(
 
 <template>
   <transition name="nested" mode="out-in">
-    <div
-      v-show="show"
-      class="fixed inset:0 z:999 flex center-content pointer-events:none"
-    >
+    <div v-show="show" class="fixed inset:0 z:999 flex center-content">
       <div
-        class="abs inset:0 bg:black translateX(0) :is(.nested-enter-active,.nested-leave-active)_{~all|.5s} .nested-enter-from_{translateX(-100%)} .nested-leave-to_{translateX(100%)}"
+        class="abs inset:0 bg:black translateX(0) :is(.nested-enter-active,.nested-leave-active)_{transition:all|1s} .nested-enter-from_{translateX(-100%)} .nested-leave-to_{translateX(100%)}"
       ></div>
       <div
-        class="abs inset:0 bg:black translateX(100%) :is(.nested-enter-active,.nested-leave-active)_{transition:all|1s} .nested-enter-from_{translateX(-100%)}"
+        class="abs inset:0 bg:black translateX(100%) :is(.nested-enter-active,.nested-leave-active)_{transition:all|1s} .nested-enter-from_{translateX(-100%)} {abs;top:0;w:50%;h:full}>svg"
       >
         <svg
-          class="abs top:0 right:0 w:auto h:full"
+          class="right:0"
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 1080 1080"
+          preserveAspectRatio="none"
         >
           <polygon
             points="900,0 0,0 180,540 0,1080 900,1080 1080,540"
@@ -39,9 +37,10 @@ watch(
           />
         </svg>
         <svg
-          class="abs top:0 left:1/2 w:auto h:full translateX(-50%)"
+          class="left:1/2 translateX(-50%)"
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 1080 1080"
+          preserveAspectRatio="none"
         >
           <polygon
             points="900,0 0,0 180,540 0,1080 900,1080 1080,540"
@@ -49,9 +48,10 @@ watch(
           />
         </svg>
         <svg
-          class="abs top:0 left:0 w:auto h:full"
+          class="left:0"
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 1080 1080"
+          preserveAspectRatio="none"
         >
           <polygon
             points="900,0 0,0 180,540 0,1080 900,1080 1080,540"
@@ -60,12 +60,13 @@ watch(
         </svg>
       </div>
       <div
-        class="abs inset:0 bg:black translateX(-100%) :is(.nested-enter-active,.nested-leave-active)_{transition:all|1s} .nested-leave-to_{translateX(100%)}"
+        class="abs inset:0 bg:black translateX(-100%) :is(.nested-enter-active,.nested-leave-active)_{transition:all|1s} .nested-leave-to_{translateX(100%)} {abs;top:0;w:50%;h:full}>svg"
       >
         <svg
-          class="abs top:0 right:0 w:auto h:full"
+          class="right:0"
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 1080 1080"
+          preserveAspectRatio="none"
         >
           <polygon
             points="900,0 0,0 180,540 0,1080 900,1080 1080,540"
@@ -73,9 +74,10 @@ watch(
           />
         </svg>
         <svg
-          class="abs top:0 left:1/2 w:auto h:full translateX(-50%)"
+          class="left:1/2 translateX(-50%)"
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 1080 1080"
+          preserveAspectRatio="none"
         >
           <polygon
             points="900,0 0,0 180,540 0,1080 900,1080 1080,540"
@@ -83,9 +85,10 @@ watch(
           />
         </svg>
         <svg
-          class="abs top:0 left:0 w:auto h:full"
+          class="left:0"
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 1080 1080"
+          preserveAspectRatio="none"
         >
           <polygon
             points="900,0 0,0 180,540 0,1080 900,1080 1080,540"
